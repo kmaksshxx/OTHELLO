3a9bec9b19ffc699db0e605bd451a991950fa5db
kmaksshxx <kmaksshxx@gmail.com>
2026-03-07 19:19:46 +0900

    Add files via upload

---
+++ models.py
@@ -0,0 +1,53 @@
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(channels)
+
+    def forward(self, x):
+        identity = x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += identity  # skip connection
+        return F.relu(out)
+
+class OthelloResNet(nn.Module):
+    def __init__(self, num_blocks=4, channels=64):
+        super().__init__()
+
+        self.conv = nn.Conv2d(2, channels, kernel_size=3, padding=1, bias=False)
+        self.bn = nn.BatchNorm2d(channels)
+
+        self.res_blocks = nn.ModuleList([
+            ResidualBlock(channels) for _ in range(num_blocks)
+        ])
+
+        self.policy_conv = nn.Conv2d(channels, 2, kernel_size=1)
+        self.policy_bn = nn.BatchNorm2d(2)
+        self.policy_fc = nn.Linear(2 * 8 * 8, 65)
+
+        self.value_conv = nn.Conv2d(channels, 1, kernel_size=1)
+        self.value_bn = nn.BatchNorm2d(1)
+        self.value_fc1 = nn.Linear(8 * 8, 64)
+        self.value_fc2 = nn.Linear(64, 1)
+
+    def forward(self, x):
+        # shared trunk
+        x = F.relu(self.bn(self.conv(x)))
+        for block in self.res_blocks:
+            x = block(x)
+
+        # policy head
+        p = F.relu(self.policy_bn(self.policy_conv(x)))
+        p = p.view(p.size(0), -1)
+        p = self.policy_fc(p) #(B, 64)
+
+        # value head
+        v = F.relu(self.value_bn(self.value_conv(x)))
+        v = v.view(v.size(0), -1)
+        v = F.relu(self.value_fc1(v))
+        v = torch.tanh(self.value_fc2(v))
+
+        return p, v #(B, 64), (B, 1)
